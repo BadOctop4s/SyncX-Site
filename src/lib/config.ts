e1fa6a0fbf950d31f9b9a41e2e31c622348824f8
BadOctop4s <eodraxkk6@gmail.com>
2026-04-19 18:45:18 -0300

    fix

--- src/lib/config.ts
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-  discord:  process.env.NEXT_PUBLIC_DISCORD_INVITE!,
-  github:   process.env.NEXT_PUBLIC_GITHUB_ORG!,
-  zerohour: process.env.NEXT_PUBLIC_ZEROHOUR_DOWNLOAD!,
-  loadstring: process.env.NEXT_PUBLIC_ROYALHUB_LOADSTRING!,
-} as const
